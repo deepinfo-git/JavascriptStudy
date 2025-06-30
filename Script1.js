36d425e4f293c3f51cdd955d59c49ef79857faa4
hj89kim <hj89kim@gmail.com>
2025-06-30 10:07:37 +0900

    깃을 두번 사용했어요

---
+++ Script1.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + "AND" + num2 + "of SUM " + (num1 + num2) + "!");
---
+++ test.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello JS</title>
+</head>
+<body>
+  <h1>JavaScript Example</h1>
+  <p id="demo">내용 없음</p>
+
+  <script>
+    document.getElementById("demo").innerText = "Hello, World!";
+  </script>
+</body>
+</html>
